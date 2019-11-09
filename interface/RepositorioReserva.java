ddfdb19a7ac0cc140e40f9af84af9aa92139f448
natalia <natalia@NATALIA>
2019-11-09 00:37:14 -0200

    duvidaaa

--- interface/RepositorioReserva.java
+++ interface/RepositorioReserva.java
@@ -1,4 +1,15 @@
 
 public interface RepositorioReserva {
 
+	public void inserir (Reserva reserva);
+	
+	public boolean procurar (Reserva reserva); 
+	/* acho que teria que usar a classe pessoa
+	 * duvida
+	 */
+	
+	public void remover (Reserva reserva);
+	
+	public void atualizar (Reserva reservaAlterada);
+	
 }
